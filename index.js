ffe87d94777db58c7c54cb7f8e784387f4753def
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2017-11-13 11:08:06 -0600

    Axios added.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/factions', (req, res) => {
+    axios('https://esi.tech.ccp.is/latest/universe/factions')
+        .then(response => response.data)
+        .then(results => {
+            const factionArray = results.map(object => object.name)
+            res.send({"results": factionArray});
+        })
+        .catch(err => {
+            res.send(err);
+        });
+});
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -14,4 +27,4 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-console.log(`Password generator listening on ${PORT}`);
+console.log(`Server listening on ${PORT}`);
