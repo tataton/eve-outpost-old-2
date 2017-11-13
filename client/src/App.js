8fb13d499e18a8e6705bfbb52fccf79e9b965ded
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2017-11-13 11:24:03 -0600

    Testing axios.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      listFactions: []
+    }
+    this.getFactions = this.getFactions.bind(this);
+  }
+
+  getFactions () {
+    fetch('/api/factions')
+    .then(response => response.json())
+    .then(json => {
+      this.setState(() => {return {listFactions: json.results}})
+    })
+    .catch((err) => {
+      console.log("error", err);
+    });
+  }
+
   render() {
+    const members = this.state.listFactions;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Faction List</h1>
+        <button onClick={this.getFactions}>Get Factions</button>
+        <ul>
+          {members.map(factionString => 
+            <li key={factionString}>{factionString}</li>
+          )}
+        </ul>
       </div>
     );
   }
