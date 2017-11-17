085f2869e42f8587f8ff3ed3da3e0a119a402287
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2017-11-16 22:38:10 -0600

    Modularized RedisStore.

--- index.js
+++ index.js
@@ -2,56 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const helmet = require('helmet');
-const axios = require('axios');
 const passport = require('passport');
 const session = require('express-session');
-const redis = require('redis');
-const redisStore = require('connect-redis')(session);
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-const redisClientObject = {};
-if (process.env.REDIS_URL) {
-    redisClientObject.url = process.env.REDIS_URL
-} else {
-    redisClientObject.host = process.env.REDIS_HOST;
-    redisClientObject.port = process.env.REDIS_PORT;
-    if (process.env.REDIS_PASSWORD) {
-        redisClientObject.password = process.env.REDIS_PASSWORD
-    }
-}
+const sessionConfig = require('./server/config/config-session');
 
-const redisClient = redis.createClient(redisClientObject);
-
-const sessionObject = {
-    secret: process.env.SESSION_SECRET,
-    name: process.env.COOKIE_NAME,
-    store: new redisStore({client: redisClient}),
-    saveUninitialized: false,
-    resave: false,
-    cookie: {
-        httpOnly: true,
-        maxAge: (24 * 60 * 60 * 1000)
-    }
-};
-
-if (process.env.NODE_ENV == 'production') {
+if ((process.env.NODE_ENV == 'production') ||
+    (process.env.NODE_ENV == 'development')) {
     app.set('trust proxy', 1);
-    sessionObject.proxy = true;
-    sessionObject.cookie.secure = true
-} else if (process.env.NODE_ENV == 'development') {
-    // Not sure if we need this. Create-React-App dev server
-    // does set up a proxy, but it's not clear whether it impacts
-    // Express.
-    app.set('trust proxy', 1);
-    sessionObject.proxy = true
 }
-// Otherwise, if process.env.NODE_ENV == 'staging', we do nothing.
+// Otherwise, process.env.NODE_ENV == 'staging', so no proxy used.
 
 app.use(helmet());
-app.use(session(sessionObject));
+app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
 
